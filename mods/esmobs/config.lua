11c5bc78d863288d37525a813ee0e91811cc41e5
maikerumine <michaellorenger@yahoo.com>
2016-01-13 00:07:52 -0500

    Config set! time to code the order...

--- mods/esmobs/config.lua
+++ mods/esmobs/config.lua
@@ -5,4 +5,47 @@
 
 
 
+-----------------------------------------------------------------------------
+-- You decide which mobs and how many will spawn
+-----------------------------------------------------------------------------
+-- set to true to have cute, happy little aminals to ride and play with!
+esmobs.animals = true;
+
+-- if set to true,  the Mancrapft mobs like zombie, creepy, skele, and piggy will spawn.
+esmobs.mc_like_mobs = true;
+
+-- if set to true,  The Minetest monsters will spawn like sand, dirt, tree, stone monster and oerkii will spawn.
+esmobs.mt_monsters = true;
+
+-- if set to true, The good guys will spawn and you can use them to help fight monsters.
+esmobs.good_npc = true;
+
+-- if set to true, the baddies will spawn and they look like good npc!
+esmobs.bad_npc = true;
+
+-- if set to given number, You will get all the mobs in the set mob package.  (see below for configuration)
+esmobs.overall = 5;
+
+-----------------------------------------------------------------------------
+--MOB PACKAGES
+-----------------------------------------------------------------------------
+--esmobs.overall = 12;		Animals, and MT Monsters
+--esmobs.overall = 11;		MT monsters, MC like mobs, and Bad npc's.
+--esmobs.overall = 10;		Good npc, and Bad npc's.
+--esmobs.overall = 9;		MT monsters and Animals.
+--esmobs.overall = 8;		MC like mobs and Animals.
+--esmobs.overall = 7;		Good npc, Bad npc's.
+--esmobs.overall = 6;		MT Monsters, Bad npc's.
+--esmobs.overall = 5;		Animals, Good npc, MT monsters, MC like mobs, and Bad npc's. All 76 of them.
+--esmobs.overall = 4;		Animals, Good npc, MT monsters, and MC like mobs.
+--esmobs.overall = 3;		Animals, Good npc, and MT monsters.
+--esmobs.overall = 2;		Animals and good npc's.
+--esmobs.overall = 1;		Just Animals.
+
+
+
+
+
+
+
 
