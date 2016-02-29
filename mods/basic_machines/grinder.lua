b5fb826f1636f1b33020b62cb9cd1928bb71ebcd
maikerumine <michaellorenger@yahoo.com>
2016-02-29 09:17:27 -0500

    power outlet and grinder added

--- mods/basic_machines/grinder.lua
+++ mods/basic_machines/grinder.lua
@@ -1,9 +1,17 @@
--- rnd 2015:
+-- rnd 2016:
 
--- this node works as a reverse of crafting process with a 25% loss of items (aka recycling). You can select which recipe to use when recycling.
--- There is a fuel cost to recycle
+-- this node works as technic grinder
+-- There is a certain fuel cost to operate
 
-local recycler_process = function(pos) 
+-- recipe list: [in] ={fuel cost, out}
+basic_machines.grinder_recipes = {
+	["default:stone"] = {4,"default:sand"},
+	["default:cobble"] = {4,"default:gravel"},
+	["default:gravel"] = {1,"default:dirt"},
+};
+
+
+local grinder_process = function(pos) 
 	
 	local node = minetest.get_node({x=pos.x,y=pos.y-1,z=pos.z}).name;
 	local meta = minetest.get_meta(pos);local inv = meta:get_inventory();
@@ -21,7 +29,6 @@ local recycler_process = function(pos)
 		if fueladd.time == 0 then -- no fuel inserted, try look for outlet
 				-- No valid fuel in fuel list
 				supply = basic_machines.check_power({x=pos.x,y=pos.y,z=pos.z}) or 0;
-				minetest.chat_send_all("supply "..supply);
 				if supply>0 then 
 					fueladd.time = 40 -- same as 10 coal
 				else
@@ -43,72 +50,31 @@ local recycler_process = function(pos)
 
 	
 	
-	-- RECYCLING: check out inserted items
+	-- PROCESS: check out inserted items
 	local stack = inv:get_stack("src",1);
 		if stack:is_empty() then return end; -- nothing to do
 
 		local src_item = stack:to_string();
 		local pos=string.find(src_item," "); if pos then src_item = string.sub(src_item,1,pos-1) end -- take first word to determine what item was 
 		
-		-- look if we already handled this item
-		local known_recipe=true;
-		if src_item~=meta:get_string("node") then-- did we already handle this? if yes read from cache
-			meta:set_string("node",src_item);
-			meta:set_string("itemlist","{}");
-			known_recipe=false;
-		end
+		local def = basic_machines.grinder_recipes[src_item];
+		if not def then 
+			meta:set_string("infotext", "please insert valid materials"); return
+		end-- unknown node
 		
-		local itemlist;
-		if not known_recipe then
-			local recipe = minetest.get_all_craft_recipes( src_item );
-			local recipe_id = tonumber(meta:get_int("recipe")) or 1;
-			
-			if not recipe then 
-				return
-			else 
-				itemlist = recipe[recipe_id];
-				if not itemlist then meta:set_string("node","") return end;
-				itemlist=itemlist.items;
-			end
-			local output = recipe[recipe_id].output or "";
-			if string.find(output," ") then 
-				local par = string.find(output," ");
-				if (tonumber(string.sub(output, par)) or 0)>1 then itemlist = {} end
-			end -- cause  if for example output is "default:mese 9" we dont want to get meseblock from just 1 mese..
-			
-			meta:set_string("itemlist",minetest.serialize(itemlist)); -- read cached itemlist
-		else 
-			itemlist=minetest.deserialize(meta:get_string("itemlist")) or {};
-		end
+		fuel = fuel-def[1]; -- burn fuel
+		if fuel<0 then meta:set_string("infotext", "need at least " .. def[1] .. " fuel to complete operation "); return end
 		
-		--empty dst inventory before proceeding
-		-- local size = inv:get_size("dst"); 
-		-- for i=1,size do
-			-- inv:set_stack("dst", i, ItemStack(""));
-		-- end
-		
-		for _,  v in pairs(itemlist) do
-			if math.random(1, 4)<=3 then -- probability 3/4 = 75%
-				if not string.find(v,"group") then -- dont add if item described with group
-					local par = string.find(v,"\"") or 0;
-					--minetest.chat_send_all(" par location at " .. par .. " item ".. v);
-					if inv:room_for_item("dst", ItemStack(v)) then -- can item be put in
-						inv:add_item("dst",ItemStack(v));
-					else return
-					end
-				end
-			end
-		end
+		inv:add_item("dst",ItemStack(def[2]));
 	
 		--take 1 item from src inventory for each activation
 		stack=stack:take_item(1); inv:remove_item("src", stack)
 		
-		fuel = fuel-1; -- burn fuel on succesful operation
-		meta:set_float("fuel",fuel); meta:set_string("infotext", "fuel status " .. fuel .. ", recycling " .. meta:get_string("node"));
+		meta:set_float("fuel",fuel); meta:set_string("infotext", "fuel status " .. fuel);
 end
 
 
-local recycler_update_meta = function(pos)
+local grinder_update_meta = function(pos)
 		local meta = minetest.get_meta(pos);
 		local list_name = "nodemeta:"..pos.x..','..pos.y..','..pos.z 
 		local form  = 
@@ -119,23 +85,19 @@ local recycler_update_meta = function(pos)
 		"list["..list_name..";dst;1.,0.5;3,3;]"..
 		"list["..list_name..";fuel;0.,2.5;1,1;]".. 
 		"list[current_player;main;0,4;8,4;]"..
-		"field[4.5,0.75;2,1;recipe;select recipe: ;" .. (meta:get_int("recipe")) .. "]"..
 		"button[6.5,0.5;1,1;OK;OK]";
-		
-		--"field[0.25,4.5;2,1;mode;mode;"..mode.."]";
 		meta:set_string("formspec", form);
 end
 
-minetest.register_node("basic_machines:recycler", {
-	description = "Recycler",
-	tiles = {"recycler.png"},
+minetest.register_node("basic_machines:grinder", {
+	description = "Grinder",
+	tiles = {"grinder.png"},
 	groups = {oddly_breakable_by_hand=2,mesecon_effector_on = 1},
 	sounds = default.node_sound_wood_defaults(),
 	after_place_node = function(pos, placer)
 		local meta = minetest.get_meta(pos);
-		meta:set_string("infotext", "Recycler: put one item in it (src) and obtain 75% of raw materials (dst). To operate it insert fuel, then insert item to recycle or use keypad to activate it.")
+		meta:set_string("infotext", "Grinder: To operate it insert fuel, then insert item to grind or use keypad to activate it.")
 		meta:set_string("owner", placer:get_player_name());
-		meta:set_int("recipe",1);
 		meta:set_float("fuel",0);
 		local inv = meta:get_inventory();inv:set_size("src", 1);inv:set_size("dst",9);inv:set_size("fuel",1);
 	end,
@@ -144,7 +106,7 @@ minetest.register_node("basic_machines:recycler", {
 		local meta = minetest.get_meta(pos);
 		local privs = minetest.get_player_privs(player:get_player_name());
 		if meta:get_string("owner")~=player:get_player_name() and not privs.privs then return end -- only owner can interact with recycler
-		recycler_update_meta(pos);
+		grinder_update_meta(pos);
 	end,
 	
 	allow_metadata_inventory_put = function(pos, listname, index, stack, player)
@@ -163,7 +125,7 @@ minetest.register_node("basic_machines:recycler", {
 	
 	on_metadata_inventory_put = function(pos, listname, index, stack, player) 
 		if listname == "fuel" or listname =="dst" then return end -- just put fuel in, nothing else
-		recycler_process(pos);
+		grinder_process(pos);
 	end,
 	
 	allow_metadata_inventory_move = function(pos, from_list, from_index, to_list, to_index, count, player)
@@ -174,34 +136,57 @@ minetest.register_node("basic_machines:recycler", {
 		action_on = function (pos, node,ttl) 
 		if type(ttl)~="number" then ttl = 1 end
 		if ttl<0 then return end -- machines_TTL prevents infinite recursion
-		recycler_process(pos);
+		grinder_process(pos);
 	end
 	}
 	},
 	
 	on_receive_fields = function(pos, formname, fields, sender) 
-		--	minetest.chat_send_all("Player "..sender:get_player_name().." submitted fields "..dump(fields))
 		if fields.quit then return end
 		local meta = minetest.get_meta(pos);
-		local recipe=1;
-		if fields.recipe then
-			recipe = tonumber(fields.recipe) or 1;
-			else return;
-		end
-		meta:set_int("recipe",recipe);
-		meta:set_string("node",""); -- this will force to reread recipe on next use
-		recycler_update_meta(pos);
+		grinder_update_meta(pos);
 	end,
 
 })
 
 
 minetest.register_craft({
-	output = "basic_machines:recycler",
+	output = "basic_machines:grinder",
 	recipe = {
-		{"default:mese_crystal","default:mese_crystal","default:mese_crystal"},
-		{"default:mese_crystal","default:diamondblock","default:mese_crystal"},
-		{"default:mese_crystal","default:mese_crystal","default:mese_crystal"},
+		{"default:diamond","default:mese","default:diamond"},
+		{"default:mese","default:diamondblock","default:mese"},
+		{"default:diamond","default:mese","default:diamond"},
 		
 	}
 })
+
+
+
+
+-- REGISTER DUSTS
+
+
+local function register_dust(name,input_node_name,ingot,cooktime)
+	
+	minetest.register_craftitem("basic_machines:"..name.."_dust", {
+		description = name.. " dust",
+		inventory_image = "basic_machines_"..name.."_dust.png",
+	})
+	
+	basic_machines.grinder_recipes[input_node_name] = {1,"basic_machines:"..name.."_dust 2"} -- register grinder recipe
+	
+	if ingot~="" then
+		minetest.register_craft({
+			type = "cooking",
+			recipe = "basic_machines:"..name.."_dust",
+			output = ingot,
+			cooktime = cooktime
+		})
+	end
+end
+
+
+register_dust("iron","default:iron_lump","default:steel_ingot",10)
+register_dust("copper","default:copper_lump","default:copper_ingot",10)
+register_dust("gold","default:gold_lump","default:gold_ingot",10)
+register_dust("diamond","default:diamond","default:diamond",3600) -- 1hr cooking time (rougly 100 coal) to make diamond!
